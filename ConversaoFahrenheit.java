7b5919ee0b2b06a10ff89d29993a79515a391f46
João Almeida <122060420+joaorafaelvra@users.noreply.github.com>
2025-03-11 19:06:29 -0300

    Conversão Celsius para Fahrenheit

---
+++ ConversaoFahrenheit.java
@@ -0,0 +1,8 @@
+public class ConversaoFahrenheit {
+    public static void main(String[] args) {
+        int celsius = 25;
+
+        double conversao = (celsius * 9/5) + 32;
+        System.out.println(conversao);
+    }
+}
