258c811414609a58cd7fd9dfa137f585db8280ff
João Almeida <122060420+joaorafaelvra@users.noreply.github.com>
2025-03-11 15:49:19 -0300

    Add files via upload

---
+++ PerimetroCirculo.java
@@ -0,0 +1,11 @@
+public class PerimetroCirculo {
+
+    public static void main(String[] args) {
+      int raio = 5;
+      double pi = 3.14159;  
+
+      double perimetro = 2 * pi * raio;
+      
+      System.out.println("O perimetro do círculo com raio " + raio + " é " + perimetro);
+    }
+}
